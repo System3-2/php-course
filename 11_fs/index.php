f2b79434436dc13f8e44b93b74ca49a8e0ca1d79
Oloja Muyiwa <olojam4969@gmail.com>
2022-11-30 00:09:10 +0100

    added file system

--- 11_fs/index.php
+++ 11_fs/index.php
@@ -1,12 +1,18 @@
 <?php
 // Magic constants
+echo __DIR__.'<br>';
+echo __FILE__.'<br>';
 
+
+echo __LINE__.'<br>';
+
+// mkdir('test-lol');
 // Create directory
 
 // Rename directory
-
+rename('test-lol', 'test');
 // Delete directory
-
+rmdir('test');
 // Read files and folders inside directory
 
 // file_get_contents, file_put_contents
